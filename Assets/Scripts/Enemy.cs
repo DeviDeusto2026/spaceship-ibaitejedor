8c170bcc6bb51379520f54b38a23990058ad754f
ibaitejedor <i.tejedor@opendeusto.es>
2026-05-08 12:34:11 +0200

    Enemigo creado

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float velocidad = 10f;
+    public int vida = 3;
+    private Transform jugador;
+
+    void Start()
+    {
+        
+        GameObject playerObj = GameObject.FindGameObjectWithTag("Ship");
+        if (playerObj != null) jugador = playerObj.transform;
+    }
+
+    void Update()
+    {
+        if (jugador != null)
+        {
+            transform.LookAt(jugador);
+            transform.Translate(Vector3.forward * velocidad * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ammo"))
+        {
+            vida--;
+            Destroy(collision.gameObject); 
+
+            if (vida <= 0)
+            {
+                Destroy(gameObject); 
+            }
+        }
+    }
+}
